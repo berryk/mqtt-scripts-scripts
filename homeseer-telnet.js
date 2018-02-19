a88adf2265f46a4779cfe7a4bb06beddb030af59
keithwberry <keithwberry@gmail.com>
2018-02-19 12:09:42 -0500

    More error handling

--- homeseer-telnet.js
+++ homeseer-telnet.js
@@ -9,73 +9,66 @@ connect();
 function connect() {
   socket = net.createConnection(11000, 'mqtt.lan');
   log.info('Socket created.');
-  
-//   socket.on('error',function(msg){
-//     log.error('Error:',msg);
-//     log.info('Reconnecting');
-//     setTimeout(connect,5000);
-//   });
-}
 
-socket.on('data', function(data) {
+  socket.on('data', function(data) {
     //parse data from response
     if (command == "gs" && data != "ok\r\n") {
-        command = "";
-        //send a message to reset counters
-        setValue("homeseer/status","reset")
-        var devicedefns = data.toString().split("|");
-        for (var i = 0, len = devicedefns.length; i < len; i++) {
-            var device = devicedefns[i];
-            device = device.replace(/\//g, '-');
-            device = device.replace(/,,/g, ',-,');
-            var fields = device.split(",");
-            var topic = 'homeseer' + '/' + fields[4] + '/' + fields[5] + '/' + fields[3];
-            devices[fields[0]] = topic;
-            // fields[2] is status - if it's Off then 0 else 1 only publish if fields[4] = lights
-            if (fields[4] == "Lights") {
-                var value;
-                if (fields[2] == "Off") {
-                    value = 0;
-                } else {
-                    value = 100;
-                }
-                setValue(topic, value);
-            }
-            topics[topic + '/set'] = fields[0];
-            log.info('Device:' + fields[0] + " Topic:" + topic)
+      command = "";
+      //send a message to reset counters
+      setValue("homeseer/status", "reset")
+      var devicedefns = data.toString().split("|");
+      for (var i = 0, len = devicedefns.length; i < len; i++) {
+        var device = devicedefns[i];
+        device = device.replace(/\//g, '-');
+        device = device.replace(/,,/g, ',-,');
+        var fields = device.split(",");
+        var topic = 'homeseer' + '/' + fields[4] + '/' + fields[5] + '/' + fields[3];
+        devices[fields[0]] = topic;
+        // fields[2] is status - if it's Off then 0 else 1 only publish if fields[4] = lights
+        if (fields[4] == "Lights") {
+          var value;
+          if (fields[2] == "Off") {
+            value = 0;
+          } else {
+            value = 100;
+          }
+          setValue(topic, value);
         }
+        topics[topic + '/set'] = fields[0];
+        log.info('Device:' + fields[0] + " Topic:" + topic)
+      }
     } else {
-        var fields = data.toString().split(",");
-        if (fields[0] == "DC") {
-            var topic = devices[fields[1]];
-            var value = fields[2];
-            log.info(topic + ":" + value);
-            setValue(topic, value);
-        }
+      var fields = data.toString().split(",");
+      if (fields[0] == "DC") {
+        var topic = devices[fields[1]];
+        var value = fields[2];
+        log.info(topic + ":" + value);
+        setValue(topic, value);
+      }
     }
-}).on('connect', function() {
+  }).on('connect', function() {
     log.info('CONNECTED');
     command = "au,default,default";
-    socket.write(command + "\r\n", function(){
-        command = "gs";
-        socket.write(command + "\r\n", function() {
-            log.info("gs\n");
-        });
+    socket.write(command + "\r\n", function() {
+      command = "gs";
+      socket.write(command + "\r\n", function() {
+        log.info("gs\n");
+      });
     });
 
-}).on('end', function() {
+  }).on('end', function() {
     log.info('DONE, reconnecting');
-    setTimeout(connect,5000);
-}).on('error',function(msg){
-    log.error('Error:',msg);
+    setTimeout(connect, 5000);
+  }).on('error', function(msg) {
+    log.error('Error:', msg);
     log.info('Reconnecting');
-    setTimeout(connect,5000);
-});
+    setTimeout(connect, 5000);
+  });
 
-
+}
 
 // Every hour rerun the gs command
-schedule('*/30 * * * *', function(){
+schedule('*/30 * * * *', function() {
   log.info('Getting full homeseer status');
   command = "gs";
   socket.write(command + "\r\n", function() {
@@ -84,11 +77,11 @@ schedule('*/30 * * * *', function(){
 });
 
 subscribe('homeseer/+/+/+/set', function(topic, val) {
-    log.info(topic + ':' + val);
+  log.info(topic + ':' + val);
 
-    var deviceid = topics[topic];
-    command = "cv," + deviceid + "," + val;
-    log.info("Writing Command:" + command);
-    socket.write(command + "\r\n");
+  var deviceid = topics[topic];
+  command = "cv," + deviceid + "," + val;
+  log.info("Writing Command:" + command);
+  socket.write(command + "\r\n");
 
 });
