428d982110b79adaacfc43742991908f85c04ddb
keithwberry <keithwberry@gmail.com>
2018-02-19 20:21:00 -0500

    Added a Q for socket calls

--- homeseer-telnet.js
+++ homeseer-telnet.js
@@ -4,6 +4,7 @@ var devices = [];
 var topics = [];
 var lights = [];
 var socket;
+var command_q = [];
 
 connect();
 
@@ -13,71 +14,78 @@ function connect() {
 
   socket.on('data', function(data) {
     //parse data from response
-//    log.info("Data:"+data);
-    
-    if (command == "au,default,default" && data == "ok\r\n")
-    {
-       if (data == "ok\r\n"){
-          log.info("Logged in successfully");
-          command = "gs";
-          socket.write(command + "\r\n");
-       } else {
-         log.info("Log in failed, command:" + command + " Data: " + data);
-         setTimeout(socket.write, 5000, command + "\r\n");
-       }
-    }
-    
-    if (command == "gs" && data != "ok\r\n") {
-      command = "";
-      //send a message to reset counters
+    //    log.info("Data:"+data);
 
-      var devicedefns = data.toString().split("|");
-      var len = devicedefns.length; 
-      
-      
-      var light_count = 0; 
-      
-      for (var i = 0; i < len; i++) {
-        var device = devicedefns[i];
-        device = device.replace(/\//g, '-');
-        device = device.replace(/,,/g, ',-,');
-        var fields = device.split(",");
-        var topic = 'homeseer' + '/' + fields[4] + '/' + fields[5] + '/' + fields[3];
-        devices[fields[0]] = topic;
-        // fields[2] is status - if it's Off then 0 else 1 only publish if fields[4] = lights
-        if (fields[4] == "Lights") {
-          var value;
-          if (fields[2] == "Off") {
-            value = 0;
-          } else {
-            value = 100;
-          }
-          light_count = light_count + 1; 
-          lights[topic] = value;
-          //setValue(topic, value);
-        }
-        topics[topic + '/set'] = fields[0];
-        log.info('Device:' + fields[0] + " Topic:" + topic)
+    if (data == "ok\r\n") {
+      var processed = command_q.shift;
+      log.info("Command:" + processed + " OK");
+
+      if (processed == "au,default,default") {
+        addCommand("gs");
       }
-      
-      setValue("homeseer/status", lights);
-      for (var light_topic in lights ){
-        setValue( light_topic, lights[light_topic]);
+
+      if (command_q.length > 0) {
+        socket.write(command_q[0]);
       }
-      
     } else {
-      var fields = data.toString().split(",");
-      if (fields[0] == "DC") {
-        var topic = devices[fields[1]];
-        var value = fields[2];
-        log.info(topic + ":" + value);
-        setValue(topic, value);
+      if (command_q[0] == "gs") {
+        // process gs
+        command_q.shift;
+
+        var devicedefns = data.toString().split("|");
+        var len = devicedefns.length;
+
+
+        var light_count = 0;
+
+        for (var i = 0; i < len; i++) {
+          var device = devicedefns[i];
+          device = device.replace(/\//g, '-');
+          device = device.replace(/,,/g, ',-,');
+          var fields = device.split(",");
+          var topic = 'homeseer' + '/' + fields[4] + '/' + fields[5] + '/' + fields[3];
+          devices[fields[0]] = topic;
+          // fields[2] is status - if it's Off then 0 else 1 only publish if fields[4] = lights
+          if (fields[4] == "Lights") {
+            var value;
+            if (fields[2] == "Off") {
+              value = 0;
+            } else {
+              value = 100;
+            }
+            light_count = light_count + 1;
+            lights[topic] = value;
+            //setValue(topic, value);
+          }
+          topics[topic + '/set'] = fields[0];
+          log.info('Device:' + fields[0] + " Topic:" + topic)
+        }
+
+        setValue("homeseer/status", lights);
+        for (var light_topic in lights) {
+          setValue(light_topic, lights[light_topic]);
+        }
+
+
+        // end process gs
+      } else {
+        // process DC status messages
+        var fields = data.toString().split(",");
+        if (fields[0] == "DC") {
+          var topic = devices[fields[1]];
+          var value = fields[2];
+          log.info("Event from homeseer:" + topic + ":" + value);
+          setValue(topic, value);
+          // end process DC status messages
+        } else {
+          log.info("!! Unknown message:" + data);
+        }
       }
     }
   }).on('connect', function() {
     log.info('CONNECTED');
     command = "au,default,default";
-    socket.write(command + "\r\n");
+    addCommand(command);
     log.info("Logging In");
   }).on('end', function() {
     log.info('DONE, reconnecting');
@@ -94,9 +102,7 @@ function connect() {
 schedule('*/30 * * * *', function() {
   log.info('Getting full homeseer status');
   command = "gs";
-  socket.write(command + "\r\n", function() {
-    log.info("gs\n");
-  });
+  addCommand(command);
 });
 
 subscribe('homeseer/+/+/+/set', function(topic, val) {
@@ -105,6 +111,12 @@ subscribe('homeseer/+/+/+/set', function(topic, val) {
   var deviceid = topics[topic];
   command = "cv," + deviceid + "," + val;
   log.info("Writing Command:" + command);
-  socket.write(command + "\r\n");
-
+  addCommand(command);
 });
+
+function addCommand(command) {
+  command_q.push(command + "\r\n");
+  if (command_q.length == 1) {
+    socket.write(command_q[0]);
+  }
+}
