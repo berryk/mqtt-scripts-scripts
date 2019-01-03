d92695b42f3470317be368edb5e52dbcbe8326ed
Keith Berry <keithwberry@gmail.com>
2019-01-03 03:04:38 +0000

    Volume changes

--- familyroomcc.js
+++ familyroomcc.js
@@ -20,9 +20,9 @@ subscribe('chromecast/' + ccname + '/player_state', function(topic, val) {
             var volume = 50;
             setValue('chromecast/' + ccname + '/command/volume_level', volume);
             setValue('homeseer/MeiHarmonyHub/MeiHarmonyHub/Family Room Activities/set',harmonyActivity);
-            setValue('MusicCast/' + mcname + '/volume/set', 120);
-            //setValue('MusicCast/' + mcname + '/input/set', mcinput);
-            //setValue('MusicCast/' + mcname + '/power/set', 'on');
+            setValue('MusicCast/' + mcname + '/volume/set', 125);
+            setValue('MusicCast/' + mcname + '/input/set', mcinput);
+            setValue('MusicCast/' + mcname + '/power/set', 'on');
             roompower = 1;
         }
 
@@ -37,7 +37,7 @@ subscribe('chromecast/' + ccname + '/player_state', function(topic, val) {
                 log.info("Time out, turning off");
                 roompower = 0;
                 setValue('homeseer/MeiHarmonyHub/MeiHarmonyHub/Family Room Activities/set','-1');
-                //setValue('MusicCast/' + mcname + '/power/set', 'standby');
+                setValue('MusicCast/' + mcname + '/power/set', 'standby');
             }, 300000);
 
             ccplaying = 0;
--- kitchencc.js
+++ kitchencc.js
@@ -19,7 +19,7 @@ subscribe('chromecast/' + ccname + '/player_state', function(topic, val) {
         if (roompower == 0) {
             var volume = 50;
             setValue('chromecast/' + ccname + '/command/volume_level', volume);
-            setValue('MusicCast/' + mcname + '/volume/set', 120);
+            setValue('MusicCast/' + mcname + '/volume/set', 125);
             setValue('MusicCast/' + mcname + '/input/set', mcinput);
             setValue('MusicCast/' + mcname + '/power/set', 'on');
             roompower = 1;
